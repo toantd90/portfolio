5e5e0692aa952e11a26211a1803f068ace84e3a8
Toan Tran <toantdfu@gmail.com>
2023-03-29 06:59:49 +0900

    Draft hero to HomePage

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+const Hero = () => (
+  <div className="text-white mx-auto max-w-screen-xl py-32 sm:pb-32 sm:pt-24">
+    <div className="max-w-6xl mx-auto px-5 sm:px-6">
+      <h3 className="text-2xl font-semibold dark:text-neutral-200">
+        Hi there, 👋 I&#39;m
+      </h3>
+      <h1 className="block bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-left animate-text">
+        Toan Tran
+      </h1>
+      <div>
+        <p
+          id="patterns-headline"
+          className="text-xl text-neutral-600 mb-8 dark:text-neutral-200"
+        >
+          Seior Front-End Software Engineer who love building products
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Hero
