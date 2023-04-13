53eb225c24767b9023fdb6c61b2e9d3167e512c2
Toan Tran <toantdfu@gmail.com>
2023-04-13 11:39:47 +0900

    Forward email to my email

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,76 +1,125 @@
+"use client"
+
+import { useState } from "react"
 import Image from "next/image"
 
-const Contact = () => (
-  <div id="contact" className="text-white mx-auto max-w-screen-xl py-32 sm:py-8">
-    <div className="max-w-6xl mx-auto px-5 sm:px-6">
-      <h1 className="block bg-clip-text text-transparent font-bold text-2xl sm:text-3xl lg:text-3xl tracking-tight bg-gradient-to-r from-pink-200 to-blue-500">
-        Contact
-      </h1>
-      <div className="flex flex-col-reverse justify-between items-center md:flex-row gap-x-20">
-        <div className="w-full">
-          <form action="#" className="space-y-8">
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+const Contact = () => {
+  const [subject, setSubject] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleOnSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    let data = {
+      subject,
+      email,
+      message,
+    }
+
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received")
+      if (res.status === 200) {
+        console.log("Response succeeded!")
+        setSubmitted(true)
+        setSubject("")
+        setEmail("")
+        setMessage("")
+      }
+    })
+  }
+
+  return (
+    <div id="contact" className="text-white mx-auto max-w-screen-xl py-32 sm:py-8">
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <h1 className="block bg-clip-text text-transparent font-bold text-2xl sm:text-3xl lg:text-3xl tracking-tight bg-gradient-to-r from-pink-200 to-blue-500">
+          Contact
+        </h1>
+        <div className="flex flex-col-reverse justify-between items-center md:flex-row gap-x-20">
+          <div className="w-full">
+            <form action="#" className="space-y-8">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="your_email@email.com"
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }}
+                  value={email}
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="subject"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="Let me know how I can help you"
+                  onChange={(e) => {
+                    setSubject(e.target.value)
+                  }}
+                  value={subject}
+                  required
+                />
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                >
+                  Your message
+                </label>
+                <textarea
+                  id="message"
+                  rows={6}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Leave a comment..."
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="btn-sm text-neutral-200 bg-neutral-900 hover:bg-neutral-800 dark:bg-blue-700 dark:hover:bg-blue-800 my-2 py-2 px-4 rounded inline-flex items-center"
+                onClick={handleOnSubmit}
               >
-                Your email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="your_email@email.com"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="subject"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Subject
-              </label>
-              <input
-                type="text"
-                id="subject"
-                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="Let me know how I can help you"
-                required
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="message"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-              >
-                Your message
-              </label>
-              <textarea
-                id="message"
-                rows={6}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Leave a comment..."
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="btn-sm text-neutral-200 bg-neutral-900 hover:bg-neutral-800 dark:bg-blue-700 dark:hover:bg-blue-800 my-2 py-2 px-4 rounded inline-flex items-center"
-            >
-              Send message
-            </button>
-          </form>
+                Send message
+              </button>
+            </form>
+          </div>
+          <Image
+            className="md:max-w-[50%]"
+            src="/images/get-in-touch.png"
+            alt="Get in touch"
+            width={500}
+            height={500}
+          />
         </div>
-        <Image
-          className="md:max-w-[50%]"
-          src="/images/get-in-touch.png"
-          alt="Get in touch"
-          width={500}
-          height={500}
-        />
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Contact
--- components/Icons/ArrowRight.tsx
+++ components/Icons/ArrowRight.tsx
@@ -7,7 +7,7 @@ export default function ArrowRight() {
     >
       <path
         d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-        fill-rule="nonzero"
+        fillRule="nonzero"
       ></path>
     </svg>
   )
