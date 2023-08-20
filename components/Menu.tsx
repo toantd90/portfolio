cd19a8da1818cb9a12668020428d7ccc483ee405
Toan Tran <toantdfu@gmail.com>
2023-08-20 10:29:30 +0700

    Show header on md

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -19,9 +19,9 @@ export default function Menu({ isMenuActive, onMenuClick }: Props) {
   return (
     <nav
       id="mobile-nav"
-      className={`${isMenuActive ? styles.active : ""} ${
+      className={`backdrop-blur md:opacity-100 md:visible ${isMenuActive ? styles.active : ""} ${
         styles.nav
-      } absolute top-full h-screen pb-16 z-20 left-0 w-full overflow-scroll bg-white transition-all duration-300 backdrop-blur shadow-lg ease-in-out md:flex-grow md:opacity-100 md:visible md:pb-0 md:w-auto md:overflow-hidden md:h-auto md:static md:bg-transparent md:shadow-none`}
+      } absolute top-full h-screen pb-16 z-20 left-0 w-full overflow-scroll transition-all duration-300 ease-in-out md:flex-grow  md:pb-0 md:w-auto md:overflow-hidden md:h-auto md:static`}
     >
       <ul className="px-5 py-2 md:flex flex-grow justify-end flex-wrap items-center">
         {MENUS.map(({ href, text }) => (
